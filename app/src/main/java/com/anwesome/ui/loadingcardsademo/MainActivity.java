9499aa1dbcbb7af84638ea581dff3ec10fd9acf6
Anwesh43 <anweshthecool0@gmail.com>
2017-05-18 13:34:35 +0530

    Using LoadingCardList in app

--- app/src/main/java/com/anwesome/ui/loadingcardsademo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/loadingcardsademo/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 
+import com.anwesome.ui.loadingcards.LoadingCardList;
 import com.anwesome.ui.loadingcards.LoadingCardView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,8 +12,13 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        LoadingCardView loadingCardView = new LoadingCardView(this);
-        addContentView(loadingCardView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,600));
+//        setContentView(R.layout.activity_main);
+//        LoadingCardView loadingCardView = new LoadingCardView(this);
+//        addContentView(loadingCardView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,600));
+        LoadingCardList loadingCardList = new LoadingCardList(this);
+        for(int i=0;i<4;i++) {
+            loadingCardList.addLoadingCard(null);
+        }
+        loadingCardList.show();
     }
 }
