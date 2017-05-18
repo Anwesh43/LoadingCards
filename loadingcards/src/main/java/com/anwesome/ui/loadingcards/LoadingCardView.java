ed54466772239c8b4a7c074ae16aa093956cf603
Anwesh43 <anweshthecool0@gmail.com>
2017-05-18 10:26:56 +0530

    adding loadingblock

--- loadingcards/src/main/java/com/anwesome/ui/loadingcards/LoadingCardView.java
+++ loadingcards/src/main/java/com/anwesome/ui/loadingcards/LoadingCardView.java
@@ -3,7 +3,9 @@ package com.anwesome.ui.loadingcards;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.RectF;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -38,7 +40,36 @@ public class LoadingCardView extends View {
         return true;
     }
     private class LoadingCard {
+        private float factor = 0;
+        public LoadingCard() {
+            
+        }
         public void draw(Canvas canvas) {
+        }
+        public void update() {
+            factor+=0.1f;
+            if(factor>1) {
+                factor = 0;
+            }
+        }
+    }
+    private class LoadingBlock {
+        private float x,y,w,h,mx = 0;
+        public LoadingBlock(float x,float y,float w,float h) {
+            this.x = x;
+            this.y = y;
+            this.w = w;
+            this.h = h;
+            this.mx = 0;
+        }
+        public void draw(Canvas canvas) {
+            paint.setColor(Color.parseColor("#E0E0E0"));
+            canvas.save();
+            canvas.translate(x,y);
+            canvas.drawRect(new RectF(0,0,w,h),paint);
+            paint.setColor(Color.parseColor("#757575"));
+            canvas.drawRect(new RectF(mx,0,mx+w/10,h),paint);
+            canvas.restore();
 
         }
     }
