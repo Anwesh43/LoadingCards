57f6266a0f249b5a2f32737dd1711a494335c5ee
Anwesh43 <anweshthecool0@gmail.com>
2017-05-18 14:23:58 +0530

    adding logic to fetch card properties

--- app/src/main/java/com/anwesome/ui/loadingcardsademo/MainActivity.java
+++ app/src/main/java/com/anwesome/ui/loadingcardsademo/MainActivity.java
@@ -1,9 +1,12 @@
 package com.anwesome.ui.loadingcardsademo;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ViewGroup;
 
+import com.anwesome.ui.loadingcards.CardProcessor;
 import com.anwesome.ui.loadingcards.LoadingCardList;
 import com.anwesome.ui.loadingcards.LoadingCardView;
 
@@ -15,9 +18,44 @@ public class MainActivity extends AppCompatActivity {
 //        setContentView(R.layout.activity_main);
 //        LoadingCardView loadingCardView = new LoadingCardView(this);
 //        addContentView(loadingCardView,new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,600));
+        final Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.stp);
         LoadingCardList loadingCardList = new LoadingCardList(this);
         for(int i=0;i<4;i++) {
-            loadingCardList.addLoadingCard(null);
+            final int index = i+1;
+            loadingCardList.addLoadingCard(new CardProcessor() {
+                @Override
+                public Bitmap fetchBitmap() {
+                    try {
+                        Thread.sleep(1000);
+                    }
+                    catch (Exception ex) {
+
+                    }
+                    return bitmap;
+                }
+
+                @Override
+                public String fetchTitle() {
+                    try {
+                        Thread.sleep(1000);
+                    }
+                    catch (Exception ex) {
+
+                    }
+                    return "title "+index;
+                }
+
+                @Override
+                public String fetchSubTitle() {
+                    try {
+                        Thread.sleep(1000);
+                    }
+                    catch (Exception ex) {
+
+                    }
+                    return "subtitle "+index;
+                }
+            });
         }
         loadingCardList.show();
     }
